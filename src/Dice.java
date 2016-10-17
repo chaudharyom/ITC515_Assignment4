15571d0cc9f5f014cb2118d3812bed4723ad1d40
chaudharyom <prakash21.om@gmail.com>
2016-10-17 19:44:40 +1100

    Fixed the dice value of not randomizing every turn

--- src/Dice.java
+++ src/Dice.java
@@ -1,20 +1,26 @@
-public class Dice {
-			
-	private DiceValue value;
-	
-	public Dice() {
-		value =  DiceValue.getRandom();
-	}
-	
-	public DiceValue getValue() {
-		return value;
-	}
+package ITC515_Assignment4
 
-	public DiceValue roll() {
-		return DiceValue.getRandom();
-	}		
-	
-	public String toString() {
-		return value.toString();
-	}
+public class Dice {
+
+    private DiceValue value;
+
+    // constructor to get a random value for the dice
+    public Dice() {
+        value = DiceValue.getRandom();
+    }
+
+    public DiceValue getValue() {
+        return value;
+    }
+
+    // TODO: BUG
+    // fix the bug of dice value does not change
+    public DiceValue roll() {
+        this.value = DiceValue.getRandom();
+        return this.value;
+    }
+
+    public String toString() {
+        return value.toString();
+    }
 }
